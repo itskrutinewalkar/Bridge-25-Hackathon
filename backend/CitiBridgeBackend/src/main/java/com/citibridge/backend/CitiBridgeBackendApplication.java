0f43e6ecdbad9415b82b8568cb77760b7f86ebf1
itskrutinewalkar <newalkarkruti@gmail.com>
2025-03-06 21:15:45 +0530

    created_project_structure

---
+++ backend/CitiBridgeBackend/src/main/java/com/citibridge/backend/CitiBridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.citibridge.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiBridgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiBridgeBackendApplication.class, args);
+	}
+
+}
---
+++ backend/CitiBridgeBackend/src/test/java/com/citibridge/backend/CitiBridgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citibridge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiBridgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
